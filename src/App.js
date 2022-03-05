80988e6b9c5aa3007186a8821b61468a7f70dad6
Glenn Phillips <glennp167@gmail.com>
2022-03-05 19:39:00 -0300

    New project google-front

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './googlelogo_color.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Yo</h1>
+      </header>
+      <main>
+        <img src={logo} className="App-logo"
+        alt="logo" />
+      </main>
+    </div>
+  );
+}
+
+export default App;
