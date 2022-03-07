8556e785a4ea7109c78cb0252022e0d79f72bb5b
Glenn Phillips <glennp167@gmail.com>
2022-03-07 11:04:15 -0300

    Added search.css and search.js
    Added profile icon to navbar. Need to fix px on text.

--- src/App.js
+++ src/App.js
@@ -4,25 +4,56 @@ import './App.css';
 import './/components/Container.css';
 import './/components/Navbar.css';
 import './/components/Logo.css';
+import './/components/Search.css';
+import { UserCircleIcon } from '@heroicons/react/solid';
 
 function App() {
   return (
     <div className="container">
       <header className="navbar">
         <ul>
-          <li><a href="#" alt="Gmail">Gmail</a></li>
-          <li><a href="#" alt="Images">Images</a></li>
-          <li>Icon 9</li>
-          <li>profile_pic</li>
+          <li>
+            <a href="#" alt="Gmail">
+              Gmail
+            </a>
+          </li>
+          <li>
+            <a href="#" alt="Images">
+              Images
+            </a>
+          </li>
+          <li>
+            <a class="gb_A" href="#" alt="Gapps">
+              <svg className="gb_Se" focusable="false" viewBox="0 0 24 24">
+                <path d="M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z"></path>
+              </svg>
+            </a>
+          </li>
+          <li>
+            <a className='profile-icon-link' href="#" alt="profile">
+              <UserCircleIcon className='profile-icon' />
+            </a>
+          </li>
         </ul>
       </header>
       <main>
-        <div className="logo"><img src={logo}
-        alt="logo" /></div>
-        
+        <div className="logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div class="search-container">
+          <input className="search-bar" type="search" aria-label="search site" />
+          <ul>
+            <li>
+              <button>Google Search</button>
+            </li>
+            <li>
+              <button>I'm Feeling Lucky</button>
+            </li>
+          </ul>
+        </div>
       </main>
       <footer>
-        
+
       </footer>
     </div>
   );
