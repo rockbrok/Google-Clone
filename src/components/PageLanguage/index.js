83abfbe79acf3c71eeba32d7b36ca0932f7af79b
Glenn Phillips <glennp167@gmail.com>
2022-03-24 13:46:59 -0300

    Reverted to previous version

--- src/components/PageLanguage/index.js
+++ src/components/PageLanguage/index.js
@@ -16,27 +16,13 @@ function PageLanguage() {
     )
 }
 
-const change = () => {
- arr = ["Español (Latinoamérica)", "English (United States)"];
- index = 0;
-    $('#next').click(function(){
-      $('#quote').html(arr[index])
-      index = (index + 1) % arr.length;
-    }),
-};
-
 const Language = () => (
     <div class="search-language-container">
         <div class="search-language">
             {t('google_language')}&nbsp;
-            <div id="next">
-                <a onClick={() => changeLanguage("es")}>
-                    {t('language_name')}
-                </a>
-                <a onClick={() => changeLanguage("en")}>
-                    {t('language_name')}
-                </a>
-            </div>
+            <a onClick={() => changeLanguage("es" || "en")}>
+                {t('language_name')}
+            </a>
         </div>
     </div>
 );
