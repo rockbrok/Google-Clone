66cc18df71d240a54a890265c0ef056eaae1526a
Glenn Phillips <glennp167@gmail.com>
2022-03-24 13:34:49 -0300

    Commit before adding jquery

--- src/components/PageLanguage/index.js
+++ src/components/PageLanguage/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import './style.css';
-import i18next from 'i18next';
+import i18n from 'i18next';
 import { useTranslation } from "react-i18next";
 import { t } from 'i18next';
 
 const changeLanguage = (language) => {
-    i18next.changeLanguage(language);
+    i18n.changeLanguage(language);
 };
 
 function PageLanguage() {
@@ -16,18 +16,27 @@ function PageLanguage() {
     )
 }
 
+const change = () => {
+ arr = ["Español (Latinoamérica)", "English (United States)"];
+ index = 0;
+    $('#next').click(function(){
+      $('#quote').html(arr[index])
+      index = (index + 1) % arr.length;
+    }),
+};
+
 const Language = () => (
     <div class="search-language-container">
         <div class="search-language">
-        
             {t('google_language')}&nbsp;
-
-                <a onClick={() => changeLanguage("en")}>
-                {t('language_name')}
-                </a>
+            <div id="next">
                 <a onClick={() => changeLanguage("es")}>
-                {t('language_name')}
+                    {t('language_name')}
                 </a>
+                <a onClick={() => changeLanguage("en")}>
+                    {t('language_name')}
+                </a>
+            </div>
         </div>
     </div>
 );
--- src/i18n.js
+++ src/i18n.js
@@ -1,5 +1,6 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import LanguageDetector from 'i18next-browser-languagedetector';
 import translationEN from './locales/en/translationEN.json';
 import translationES from './locales/es/translationES.json';
 
@@ -14,6 +15,7 @@ const resources = {
 
 i18n
   .use(initReactI18next)
+  .use(LanguageDetector)
   .init({
       resources,
       lng: 'en',
@@ -22,7 +24,12 @@ i18n
 
       interpolation: {
           escapeValue: false
-      }
+      },
+
+      detection: {
+          order: ['cookie', 'htmlTag', 'localStorage', 'path'],
+          caches: ['cookie'],
+      },
   });
 
   export default i18n;
