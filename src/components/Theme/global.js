636bea80eb827d5d6c6b029f2f28201b33b0452e
Glenn Phillips <glennp167@gmail.com>
2022-04-06 14:14:06 -0300

    Cleaned up global styles and dark styles. Need to fix minor coloring errors

--- src/components/Theme/global.js
+++ src/components/Theme/global.js
@@ -1,88 +1,33 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
-  *,
-  *::after,
-  *::before {
-  }
-
   :root {
+    --background-color: ${({ theme }) => theme.backgroundcolor};
     --logo: ${({ theme }) => theme.logo};
-    --scrollbar-color: ${({ theme }) => theme.scrollbarbackground};
+    --scrollbar-color: ${({ theme }) => theme.scrollbarcolor};
     --scrollbar-thumb: ${({ theme }) => theme.scrollbarthumb};
-    --gapp-background-color: ${({ theme }) => theme.gabackground};
-    --gapp-thumb-color: ${({ theme }) => theme.searchbarcolor};
-    --footer-background-color: ${({ theme }) => theme.shadow1};
-    --search-button-hover: ${({ theme }) => theme.grayhover1};
-    --search-button-color: ${({ theme }) => theme.gray1};
-    --searchbar-icon: ${({ theme }) => theme.searchbariconfill};
+    --gapp-background-color: ${({ theme }) => theme.gappbackgroundcolor};
+    --footer-background-color: ${({ theme }) => theme.footerbackground};
+    --searchbar-hover: ${({ theme }) => theme.searchbuttoncolor};
+    --searchbar-border-hover: ${({ theme }) => theme.footerbackground};
+    --search-button-hover: ${({ theme }) => theme.searchbuttonhover};
+    --search-button-color: ${({ theme }) => theme.searchbuttoncolor};
+    --searchbar-icon: ${({ theme }) => theme.searchbaricon};
+    --searchbar-icon-2: ${({ theme }) => theme.searchbaricon2};
     --text-color-1: ${({ theme }) => theme.textcolor1};
     --text-color-2: ${({ theme }) => theme.textcolor2};
-    --text-color-3: ${({ theme }) => theme.buttoncolor1};
-    --background-color: ${({ theme }) => theme.background};
-    --border-color-1: ${({ theme }) => theme.searchbarcolor};
-    --border-color-2: ${({ theme }) => theme.gacolor1};
-  }
-
-  input[type="search"]:hover,
-  input[type="search"]:focus,
-  .search-bar:hover,
-  .search-bar:focus,
-  .gapps-icon-link:hover {
-    background-color: ${({ theme }) => theme.gray1};
-  }
-
-  header a {
-    color: ${({ theme }) => theme.headertextcolor};
-  }
-
-  .gapps-icon {
-    fill: ${({ theme }) => theme.gappsfill};
-  }
-
-  .gapps-icon-link:active {
-    background-color: ${({ theme }) => theme.gappsactive};
-  }
-
-  .search-bar:hover,
-  .gapps-panel {
-    box-shadow: ${({ theme }) => theme.shadow1};
-  }
-  
-  .gapps-link:hover {
-    background-color: ${({ theme }) => theme.gaitemhover};
-  }
-
-  .sign-in:hover {
-    background-color: ${({ theme }) => theme.signinhover};
-    border-color: ${({ theme }) => theme.signinhover};
-  }
-
-  [data-title="Google Apps"]::after {
-    background-color: ${({ theme }) => theme.gappslabel};
-    color: ${({ theme }) => theme.gray1};
-  }
-
-  .search-language {
-    color: ${({ theme }) => theme.searchlanguagecolor};
-  }
-
-  .search-bar:hover {
-    border-color: ${({ theme }) => theme.searchbarhoverborder};
-  }
-
-  ul.panel {
-    border-color: ${({ theme }) => theme.panelborder};
-  }
-
-  .footer-country {
-    border-color: ${({ theme }) => theme.footercountryborder};
-  }
-
-
-  
-
-  .search-icon {
-    fill: ${({ theme }) => theme.searchiconfill};
+    --text-color-3: ${({ theme }) => theme.textcolor3};
+    --text-color-4: ${({ theme }) => theme.textcolor4};
+    --border-color-1: ${({ theme }) => theme.bordercolor1};
+    --border-color-2: ${({ theme }) => theme.bordercolor2};
+    --border-color-3: ${({ theme }) => theme.bordercolor3};
+    --button-hover: ${({ theme }) => theme.buttonhover};
+    --button-active: ${({ theme }) => theme.buttonactive};
+    --gapp-icon-fill: ${({ theme }) => theme.gappsfill};
+    --gapp-icon-hover: ${({ theme }) => theme.gappiconhover};
+    --gapp-icon-active: ${({ theme }) => theme.gappiconactive};
+    --gapp-title-background: ${({ theme }) => theme.gapptitlebackground};
+    --gapp-app-hover: ${({ theme }) => theme.gappapphover};
+    --gapp-shadow: ${({ theme }) => theme.gappshadow};
   }
 `;
