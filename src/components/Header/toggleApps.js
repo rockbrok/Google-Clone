31c3f1f6f8cad2a95fa3837a3fda802ab1dd3072
Glenn Phillips <glennp167@gmail.com>
2022-04-09 21:07:49 -0300

    Added active state to onClick for gapps icon

--- src/components/Header/toggleApps.js
+++ src/components/Header/toggleApps.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import OutsideClickHandler from 'react-outside-click-handler';
 import './style.css';
 import GoogleApps from './../../components/GoogleApps/index';
@@ -6,22 +6,30 @@ import GoogleApps from './../../components/GoogleApps/index';
 export function ToggleApps() {
   const [show, setShow] = useState(false)
 
-    return ( <>
-      <li className="pl">
-        <OutsideClickHandler onOutsideClick={() => {setShow(false);}}>
-          <button onClick={() => setShow(!show)} alt="Google apps">
-              <GApps/>
-          </button>
-          {
-          show? <GoogleApps/> : null
-          }
-        </OutsideClickHandler>
-      </li>
-    </> )
+    return (
+      <>
+        <li className="pl">
+          <OutsideClickHandler onOutsideClick={() => {setShow(false);}}>
+              <button onClick={() => setShow(!show)} alt="Google apps">
+                { show ? <GAppsActive/> : <GApps/> }
+              </button>
+            { show ? <GoogleApps/> : null }
+          </OutsideClickHandler>
+        </li>
+      </>
+    )
 }
 
 const GApps = () => (
   <div className="gapps-icon-link" alt="Google apps" data-title="Google apps">
+    <svg className="gapps-icon" focusable="false" viewBox="0 0 24 24">
+      <path d="M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z"></path>
+    </svg>
+  </div>
+);
+
+const GAppsActive = () => (
+  <div className="gapps-icon-link-active active" alt="Google apps">
     <svg className="gapps-icon" focusable="false" viewBox="0 0 24 24">
       <path d="M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z"></path>
     </svg>
