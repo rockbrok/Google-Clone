f2bb412c5c48c459db6162ffbfd66bc1a806dba0
Glenn Phillips <glennp167@gmail.com>
2022-04-13 15:55:00 -0300

    Installed react router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Logo from './components/Logo/index';
 import Header from './components/Header/index';
 import PageLanguage from './components/PageLanguage/index';
 import Footer from './components/Footer/index';
 import Form from './components/Form/index';
+import PageNotFound from './components/404/index';
 import { useTranslation } from 'react-i18next';
 import './App.css';
+
 
 function App() {
   const { t } = useTranslation()
@@ -17,6 +20,12 @@ function App() {
         <PageLanguage />
       </main>
     <Footer />
+    <Router>
+      <Routes>
+        <Route exact path="/" />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </Router>
   </> );
 }
 
---
+++ src/components/404/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function PageNotFound() {
+  return (
+    <div className="notfound-container">
+        <b>404</b>
+        <p>Page not found</p>
+    </div>
+  )
+}
