0636d21067bc18bf0bd2ee678b49c0f9b9cb2570
Glenn Phillips <glennp167@gmail.com>
2022-05-01 20:03:22 -0300

    Merged register.js test with sign up page

--- src/components/SignPage/ShowPassword/index.jsx
+++ src/components/SignPage/ShowPassword/index.jsx
@@ -3,11 +3,17 @@ import { t } from 'i18next';
 import './style.css';
 
 function showPassword() {
-  var x = document.getElementById("password", "password2");
+  var x = document.getElementById("password");
   if (x.type === "password") {
     x.type = "text";
   } else {
     x.type = "password";
+  }
+  var y = document.getElementById("password2");
+  if (y.type === "password") {
+    y.type = "text";
+  } else {
+    y.type = "password";
   }
 }
 
