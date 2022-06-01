6fcf7bb271ccb7804544094be0f598874a1e2ef8
paq000 <glennp167@gmail.com>
2022-06-01 18:42:14 -0300

    Updated name page

--- src/pages/MyAccount/Name/index.jsx
+++ src/pages/MyAccount/Name/index.jsx
@@ -13,26 +13,26 @@ import './style.css';
 export default function Name() {
   const { setUser } = useContext(UserContext);
 
-  const {register, handleSubmit, watch, formState: { errors }} = useForm({
-    defaultValues: {
-      firstName: '',
-      lastName: ''
-    },
-    mode: 'onSubmit',
-    reValidateMode: 'onChange',
-    delayError: 1000,
-  });
-
-  const [value, setValue] = useState({
-    firstName: '',
-    lastName: ''
-  })
-
   const obj = JSON.parse(localStorage.getItem('user'));
   const email = obj[0].email;
   const firstName = obj[0].firstName;
   const lastName = obj[0].lastName;
   const URL = "http://localhost:5000/users?email=" + email;
+
+  const [value, setValue] = useState({
+    firstName: firstName,
+    lastName: lastName
+  })
+
+  const {register, handleSubmit, watch, formState: { errors }} = useForm({
+    defaultValues: {
+      firstName: value.firstName,
+      lastName: value.lastName
+    },
+    mode: 'onSubmit',
+    reValidateMode: 'onChange',
+    delayError: 1000,
+  });
 
   const currentUser = async () => {
     let response = await axios.get(URL);
@@ -74,48 +74,35 @@ export default function Name() {
       </Helmet>
         <AccountHeader />
         <div className="form-bord-bot" />
-          <section className="account-page-row-blue">
-
-              <SubHeading />
-              <section className="blue-form-flex">
-
-                  <div className="account-flex-form">
-                    <span>Changes to your name will be reflected across your Google Account.&nbsp;</span>
-                    <a href="https://support.google.com/accounts/answer/27442" target="_blank" rel="noreferrer" className="learn-more">Learn more.</a>
-                    <form onSubmit={handleSubmit(onSubmit)} noValidate>
-                      <FirstName
-                        value={value}
-                        firstName={firstName}
-                        handleChange={handleChange}
-                        register={register}
-                        errors={errors}
-                      />
-                      <LastName
-                        value={value}
-                        lastName={lastName}
-                        handleChange={handleChange}
-                        register={register}
-                        errors={errors}
-                      />
-                      
-                    <h3>Who can see your name</h3>
-                    <div className="users-row">
-                      <UsersIcon className="users-icon"/>
-                      <div className="users-note">
-                        <span>Anyone can see this info when they communicate with you or view content you create in Google services.&nbsp;</span>
-                        <a href="https://support.google.com/accounts/answer/6304920" target="_blank" rel="noreferrer" className="learn-more">Learn more.</a>
-                      </div>
-                    </div>
-
-                  <div className="form-button-row">
-                    <Cancel />
-                    <button type="submit" className="next">Save</button>
-                    </div>
-                    </form>
-                  </div>
-              </section>
-
-
+        <section className="account-page-row-blue">
+          <SubHeading />
+          <section className="blue-form-flex">
+            <section className="account-flex-form">
+              <TopNote />
+              <form onSubmit={handleSubmit(onSubmit)} noValidate>
+                <FirstName
+                  value={value}
+                  firstName={firstName}
+                  handleChange={handleChange}
+                  register={register}
+                  errors={errors}
+                />
+                <LastName
+                  value={value}
+                  lastName={lastName}
+                  handleChange={handleChange}
+                  register={register}
+                  errors={errors}
+                />
+                <Note />
+                <Buttons 
+                  firstName={firstName}
+                  lastName={lastName}
+                  value={value}
+                />
+              </form>
+            </section>
+          </section>
         </section>
     </>
   )
@@ -129,7 +116,13 @@ const SubHeading = () => (
         <div className="account-subhead-text">Name</div>
       </div>
     </div>
+  </div>
+)
 
+const TopNote = () => (
+  <div className="top-note">
+    Changes to your name will be reflected across your Google Account.&nbsp;
+    <a href="https://support.google.com/accounts/answer/27442" target="_blank" rel="noreferrer" className="learn-more">More information</a>
   </div>
 )
 
@@ -148,7 +141,6 @@ const FirstName = ({ register, firstName, handleChange, value, errors }) => (
       autoComplete="off"
       name="firstName"
       type="text"
-      placeholder={firstName}
       value={value.firstName}
       onChange={handleChange}
       className={(
@@ -182,7 +174,6 @@ const LastName = ({ register, lastName, handleChange, value, errors }) => (
       autoComplete="off"
       name="lastName"
       type="text"
-      placeholder={lastName}
       value={value.lastName}
       onChange={handleChange}
       className={(
@@ -201,10 +192,26 @@ const LastName = ({ register, lastName, handleChange, value, errors }) => (
   </div>
 )
 
-const Cancel = () => (
-  <button>
-    <Link to="/myaccount/personalinfo">
-      Cancel
-    </Link>
-  </button>
+const Note = () => (
+  <>
+    <div className="header-note">Who can see your name</div>
+      <div className="users-row">
+        <UsersIcon className="users-icon"/>
+        <div className="users-note top-note">
+          Anyone can see this info when they communicate with you or view content you create in Google services.&nbsp;
+          <a href="https://support.google.com/accounts/answer/6304920" target="_blank" rel="noreferrer" className="learn-more">Learn more.</a>
+        </div>
+      </div>
+  </>
+)
+
+const Buttons = ({ firstName, lastName, value }) => (
+  <div className="form-button-row">
+    <button className="cancel">
+      <Link to="/myaccount/personalinfo/" className="cancel-link no-deco">
+        Cancel
+      </Link>
+    </button>
+    <button type="submit" className="next" disabled={ firstName === value.firstName && lastName === value.lastName }>Save</button>
+  </div>
 )
