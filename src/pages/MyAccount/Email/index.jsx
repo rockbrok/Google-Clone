9d1fa4fa44d79b0ab7de8768dc57991193ad3e01
paq000 <glennp167@gmail.com>
2022-06-02 16:15:27 -0300

    Added email page

--- src/pages/MyAccount/Email/index.jsx
+++ src/pages/MyAccount/Email/index.jsx
@@ -1,41 +1,59 @@
+import AccountHeader from "../Header";
+import axios from "axios";
 import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
-import { useState } from "react";
+import { useState, useContext } from 'react';
+import { UserContext } from "../../../usercontext";
 import { BackArrow } from "../Home";
 import { Helmet } from "react-helmet";
-import AccountFooter from "../../../components/AccountFooter";
-import AccountHeader from "../Header";
 
 import '../style.css';
 
 export default function Email() {
-  const {register, handleSubmit, watch, formState: { errors }} = useForm({
+  const { setUser } = useContext(UserContext);
+
+  const obj = JSON.parse(localStorage.getItem('user'));
+  const email = obj[0].email;
+  const URL = "http://localhost:5000/users?email=" + email;
+
+  const [value, setValue] = useState({
+    email: email,
+  })
+
+  const {register, handleSubmit, formState: { errors }} = useForm({
     defaultValues: {
-      firstName: '',
-      lastName: '',
-      email: '',
-      password: '',
-      passwordConfirm: '',
-      month: '',
-      gender: '',
-      day: '',
-      year: '',
+      email: value.email,
     },
     mode: 'onSubmit',
     reValidateMode: 'onChange',
     delayError: 1000,
   });
 
-  const [value, setValue] = useState({
-    firstName: '',
-    lastName: '',
-    email: '',
-    password: '',
-    month: '',
-    gender: '',
-    day: '',
-    year: '',
-  })
+  const currentUser = async () => {
+    let response = await axios.get(URL);
+    return response.data;
+  };
+
+  const onSubmit = async() => {
+    try {
+      await axios({
+        method: "put",
+        url: URL,
+        data: {
+          firstName: value.firstName,
+          lastName: value.lastName,  
+        },
+        headers: { "Content-Type": "application/json"},
+      });
+      setTimeout(function () {
+        window.location.href = "http://localhost:3000/myaccount/personalinfo/";
+      }, 250);
+    } catch(error) {
+      console.log(error.response.data)
+    }
+    const user = await currentUser();
+      setUser(user);
+  }
 
   const handleChange = (e) => {
     setValue({
@@ -43,13 +61,6 @@ export default function Email() {
       [e.target.name]: e.target.value
     });
   }
-
-  const obj = JSON.parse(localStorage.getItem('user'));
-  const email = obj[0].email;
-  const firstName = obj[0].firstName;
-  const lastName = obj[0].lastName;
-  const letter = firstName.charAt(0).toUpperCase();
-  const URL = "http://localhost:5000/users?email=" + email;
 
   const validEmailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/; // user@domain.com //
   const noUsernameRegex = /^@\w+([.-]?\w+)*$/; // @hello //
@@ -66,70 +77,35 @@ export default function Email() {
         <title>Email</title>
       </Helmet>
       <AccountHeader />
-      <SubHeading />
-      <section className="account-flex-2">
-        <div className="account-flex-sect">
-        The address used to identify your Google Account to you and others.&nbsp;
-          <a className="learn-more">
-            Learn more
-          </a>
-        </div>
-        <div className="account-flex-form">
-    <div className="birthday-row">
-      <h3>{email}</h3>
-      PencilIcon
-    </div>
-
-    <h3>Change Google Account email</h3>
-    <form onSubmit={handleSubmit} noValidate>
-    <div className="input-container">
-        <input 
-          {...register("email", {
-            required: true,
-            validate: {
-              noUsername: (value) => noUsernameRegex.test(value) ? false : true, // @domain //
-              noUsernameDot: (value) => noUsernameDotRegex.test(value) ? false : true, // @domain. //
-              onlyCharacters: (value) => onlyCharactersRegex.test(value) ? false : true, // abcdefg //
-              validEmail: (value) => validEmailRegex.test(value) ? true : false, // user@domain.com //
-              noDomain: (value) => noDomainRegex.test(value) ? false : true, // user@ //
-              noAt: (value) => noAtRegex.test(value) ? false : true, // user. //
-              noAtWithTopDomain : (value) => noAtWithTopDomainRegex.test(value) ? false : true, // user.com // 
-              emptyString: (value) => emptyStringRegex.test(value) ? false : true, // ' ' //
-            }
-          })}
-          autoComplete="off"
-          name="email"
-          type="email"
-          value={value.email}
-          onChange={handleChange}
-          className={errors.email?.type !== "validEmail" && (
-            errors.email?.type !== "noUsername" ||
-            errors.email?.type !== "noUsernameDot" ||
-            errors.email?.type !== "onlyCharacters" ||
-            errors.email?.type !== "noDomain" ||
-            errors.email?.type !== "required" ||
-            errors.email?.type !== "noAt" ||
-            errors.email?.type !== "emptyString" ||
-            errors.email?.type !== "noAtWithTopDomain")
-            ? "signup-input-error" : "signup-input"
-          }
-          aria-invalid={errors.email ? "true" : "false"}
-        />
-        <span className="signup-input-placeholder">
-          New email
-        </span>
-      </div>
-       
-    </form>
-    <button>
-        <Link to="/myaccount/email/">
-          Cancel
-        </Link>
-      </button>
-      <button type="submit">Save</button>
-      </div>
+      <div className="form-bord-bot" />
+      <section className="account-page-row-blue">
+        <SubHeading />
+        <section className="account-flex-form-wrap">
+          <SubHeadNote />
+          <section className="account-flex-form">
+            <form onSubmit={handleSubmit(onSubmit)} noValidate>
+              <EmailInput 
+                register={register}
+                value={value}
+                errors={errors}
+                handleChange={handleChange}
+                noUsernameRegex={noUsernameRegex}
+                noUsernameDotRegex={noUsernameDotRegex}
+                onlyCharactersRegex={onlyCharactersRegex}
+                validEmailRegex={validEmailRegex}
+                noDomainRegex={noDomainRegex}
+                noAtWithTopDomainRegex={noAtWithTopDomainRegex}
+                noAtRegex={noAtRegex}
+                emptyStringRegex={emptyStringRegex}
+              />
+              <Buttons 
+                email={email}
+                value={value}
+              />
+            </form>
       </section>
-      <AccountFooter />
+        </section>
+       </section>
     </>
   )
 }
@@ -142,5 +118,63 @@ const SubHeading = () => (
         <div className="account-subhead-text">Email</div>
       </div>
     </div>
+  </div>
+)
+
+const SubHeadNote = () => (
+  <div className="top-note subhead-note font-16">
+    The address used to identify your Google Account to you and others.&nbsp;
+    <a href="https://support.google.com/accounts/answer/27442" target="_blank" rel="noreferrer" className="learn-more">More information</a>
+  </div>
+)
+
+const EmailInput = ({ register, value, errors, handleChange, noUsernameRegex, noUsernameDotRegex, onlyCharactersRegex, validEmailRegex, noDomainRegex, noAtWithTopDomainRegex, noAtRegex, emptyStringRegex }) => (
+  <div className="input-container">
+  <input 
+    {...register("email", {
+      required: true,
+      validate: {
+        noUsername: (value) => noUsernameRegex.test(value) ? false : true, // @domain //
+        noUsernameDot: (value) => noUsernameDotRegex.test(value) ? false : true, // @domain. //
+        onlyCharacters: (value) => onlyCharactersRegex.test(value) ? false : true, // abcdefg //
+        validEmail: (value) => validEmailRegex.test(value) ? true : false, // user@domain.com //
+        noDomain: (value) => noDomainRegex.test(value) ? false : true, // user@ //
+        noAt: (value) => noAtRegex.test(value) ? false : true, // user. //
+        noAtWithTopDomain : (value) => noAtWithTopDomainRegex.test(value) ? false : true, // user.com // 
+        emptyString: (value) => emptyStringRegex.test(value) ? false : true, // ' ' //
+      }
+    })}
+    autoComplete="off"
+    name="email"
+    type="email"
+    value={value.email}
+    onChange={handleChange}
+    className={errors.email?.type !== "validEmail" && (
+      errors.email?.type !== "noUsername" ||
+      errors.email?.type !== "noUsernameDot" ||
+      errors.email?.type !== "onlyCharacters" ||
+      errors.email?.type !== "noDomain" ||
+      errors.email?.type !== "required" ||
+      errors.email?.type !== "noAt" ||
+      errors.email?.type !== "emptyString" ||
+      errors.email?.type !== "noAtWithTopDomain")
+      ? "change-form-input-error" : "change-form-input"
+    }
+    aria-invalid={errors.email ? "true" : "false"}
+  />
+  <div className="change-form-placeholder">
+    New email
+  </div>
+</div>
+)
+
+const Buttons = ({ email, value }) => (
+  <div className="form-button-row">
+    <button className="cancel">
+      <Link to="/myaccount/personalinfo/" className="cancel-link no-deco">
+        Cancel
+      </Link>
+    </button>
+    <button type="submit" className="next" disabled={ email === value.email }>Save</button>
   </div>
 )
