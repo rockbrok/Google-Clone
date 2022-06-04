66bde5210ae79e6ab765f9048310024ca5fba2fe
paq000 <glennp167@gmail.com>
2022-06-04 08:39:33 -0300

    Updated search form to use react-hook-form

--- src/pages/Search/Form/index.jsx
+++ src/pages/Search/Form/index.jsx
@@ -1,99 +1,97 @@
 import { useState } from 'react';
 import { SearchIcon, MicrophoneIcon, XIcon } from '@heroicons/react/solid';
 import { t } from 'i18next';
+import { useForm } from 'react-hook-form';
 
 import './SearchButton/style.css';
 import './SearchBar/style.css';
 
-export default function Form() {
-  const [state, setState] = useState({
-    searchInput: '',
-    searchInputValid: false,
-    formValid: false
+export default function NewForm() {
+  const [value, setValue] = useState({
+    searchInput: ''
   });
-  
-  const initialState = { searchInput: '' }
 
-  const handleFormReset = () => {
-    setState(() => initialState)
-  }
+  const {register, handleSubmit } = useForm({
+    defaultValues: {
+      searchInput: ''
+    },
+    mode: 'onSubmit',
+    reValidateMode: 'onChange',
+    delayError: 0,
+  });
 
-  const handleUserInput = (e) => {
-    const name = e.target.className;
-    const value = e.target.value;
-    setState({[name]: value},
-      () => { validateField(name, value.trim()) });
-  }
-
-  const validateField = (fieldName, value) => {
-    let searchInputValid = state.searchInputValid;
-
-    switch(fieldName) {
-      case 'searchInput':
-        searchInputValid = value.length >= 1;
-        break;
-      default:
-        break;
-    }
-    setState({searchInputValid: searchInputValid,
-    }, validateForm);
-  }
-
-  const validateForm = () => {
-    setState({formValid: state.searchInputValid});
+  const handleReset = () => {
+    setValue(() => '')
   }
 
   return (
-    <form name="search" method="get" onReset={handleFormReset} action="https://www.google.com/search">
+    <form onSubmit={handleSubmit} onReset={handleReset} method="get" action="https://www.google.com/search">
       <div className="search-container">
         <div className="search-bar">
           <SearchIcon className="search-icon" />
-          <input
-            type="search"
-            title={t('search.searchbar_title')}
-            className="searchInput"
-            name="q"
-            role="search"
-            placeholder=" "
-            maxLength="2048"
-            autoComplete="off"
-            autoCorrect="off"
-            spellCheck="false"
-            aria-label="Search"
-            value={state.searchInput}
-            onChange={handleUserInput}
+          <Form
+            value={value}
+            setValue={setValue}
+            register={register}
           />
           <ClearButton />
           <VoiceButton />
         </div>
       </div>
-      <div className="search-button">
-        <ul>
-          <li>
-            <button className="google-search" name="" type="submit" disabled={state.formValid}>
-              {t('search.google_search')}
-            </button>
-          </li>
+        <ul className="search-button">
+          <GoogleSearch />
           <LuckySearch />
         </ul>
-      </div>
     </form>
   )
 }
 
+const Form = ({ register, value }) => (
+  <input
+    {...register("password", {
+      required: true,
+      minLength: 1,
+      validate: (value) => {
+        return !!value.trim()
+      }
+    })}
+    type="search"
+    title={t('search.searchbar_title')}
+    className="searchInput"
+    name="q"
+    role="search"
+    placeholder=" "
+    maxLength="2048"
+    autoComplete="off"
+    spellCheck="false"
+    aria-label="Search"
+    setValue={value.searchInput}
+  />
+)
+
+const GoogleSearch = () => (
+  <li>
+    <button className="google-search" name="" type="submit">
+      {t('search.google_search')}
+    </button>
+  </li>
+)
+
 const LuckySearch = () => (
   <li>
-  <button className="lucky-search" name="btnI" type="submit">
-    {t('search.lucky_search')}
-  </button>
-</li>
+    <button className="lucky-search" name="btnI" type="submit">
+      {t('search.lucky_search')}
+    </button>
+  </li>
 );
 
 const ClearButton = () => (
   <div className="clear-button">
     <button className="clear tip" type="reset" value='reset' aria-label="clear">
       <XIcon className="x-icon" />
-      <span className="clear">{t('search.clear')}</span>
+      <span className="clear">
+        {t('search.clear')}
+      </span>
     </button>
   </div>
 );
@@ -101,6 +99,8 @@ const ClearButton = () => (
 const VoiceButton = () => (
   <a href="https://www.google.com/" type="" className="tip" aria-label="search by voice">
     <MicrophoneIcon className="microphone-icon" />
-    <span className="microphone">{t('search.search_by_voice')}</span>
+    <span className="microphone">
+      {t('search.search_by_voice')}
+    </span>
   </a>
 );
